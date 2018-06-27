ba2938ab025ce44ff1183c0c7e959b8238bd42a0
Lan Xiao <lx223@users.noreply.github.com>
2018-06-28 00:28:16 +0100

    Add constants

---
+++ ios/werewolf-assistant/werewolf-assistant/resources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  werewolf-assistant
+//
+//  Created by Lan Xiao on 23/06/2018.
+//  Copyright © 2018 Lan Xiao. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let serverAddress = "localhost:21806"
+}
