15f91d6634446fe559b5e0e3621cf13758994933
Lan Xiao <lx223@users.noreply.github.com>
2018-06-28 09:48:08 +0100

    Add travis ci configuration (#1)

---
+++ ios/werewolf-assistant/werewolf-assistant/resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  werewolf-assistant
+//
+//  Created by Lan Xiao on 23/06/2018.
+//  Copyright © 2018 Lan Xiao. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let serverAddress = "localhost:21806"
+    static let googleAPIKey = ""
+}
