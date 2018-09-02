d435c9107c489ff71c65da6167af113eb6ed9502
Lan Xiao <lx223@users.noreply.github.com>
2018-09-03 00:28:05 +0100

    [iOS] Simplify rx extensions to game service client with generics

--- ios/werewolf-assistant/werewolf-assistant/extensions/GameServiceExtensions.swift
+++ ios/werewolf-assistant/werewolf-assistant/extensions/GameServiceExtensions.swift
@@ -8,65 +8,28 @@
 
 import Foundation
 import RxSwift
+import SwiftGRPC
 
 extension Werewolf_GameServiceService {
     func takeActionRx(_ req: Werewolf_TakeActionRequest) -> Observable<Werewolf_TakeActionResponse> {
-        return Observable<Werewolf_TakeActionResponse>.create { observer -> Disposable in
-            let call = try? self.takeAction(req) {
-                guard let res = $0 else {
-                    observer.onError($1)
-                    return
-                }
-
-                observer.onNext(res)
-                observer.onCompleted()
-            }
-
-            return Disposables.create {
-                call?.cancel()
-            }
-        }
+        return rxUnary(req, self.takeAction)
     }
 
     func takeSeatRx(_ req: Werewolf_TakeSeatRequest) -> Observable<Werewolf_TakeSeatResponse> {
-        return Observable<Werewolf_TakeSeatResponse>.create { observer -> Disposable in
-            let call = try? self.takeSeat(req) {
-                guard let res = $0 else {
-                    observer.onError($1)
-                    return
-                }
-
-                observer.onNext(res)
-                observer.onCompleted()
-            }
-
-            return Disposables.create {
-                call?.cancel()
-            }
-        }
+        return rxUnary(req, self.takeSeat)
     }
 
     func createAndJoinRoomRx(_ req: Werewolf_CreateAndJoinRoomRequest) -> Observable<Werewolf_CreateAndJoinRoomResponse> {
-        return Observable<Werewolf_CreateAndJoinRoomResponse>.create { observer -> Disposable in
-            let call = try? self.createAndJoinRoom(req) {
-                guard let res = $0 else {
-                    observer.onError($1)
-                    return
-                }
-
-                observer.onNext(res)
-                observer.onCompleted()
-            }
-
-            return Disposables.create {
-                call?.cancel()
-            }
-        }
+        return rxUnary(req, self.createAndJoinRoom)
     }
 
     func joinRoomRx(_ req: Werewolf_JoinRoomRequest) -> Observable<Werewolf_JoinRoomResponse> {
-        return Observable<Werewolf_JoinRoomResponse>.create { observer -> Disposable in
-            let call = try? self.joinRoom(req) {
+        return rxUnary(req, self.joinRoom)
+    }
+
+    private func rxUnary<T, K>(_ req: T, _ asyncRPC: @escaping (_ request: T, _ completion: @escaping (K?, CallResult) -> Void) throws -> ClientCallUnary) -> Observable<K> {
+        return Observable<K>.create { observer -> Disposable in
+            let call = try? asyncRPC(req) {
                 guard let res = $0 else {
                     observer.onError($1)
                     return
