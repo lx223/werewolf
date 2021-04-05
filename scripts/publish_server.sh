9fce30891f1606bfa33d08728e1fc5177bf8b60d
Lan Xiao <lx223@users.noreply.github.com>
2021-04-05 17:03:36 +0100

    Add postsubmit to release on merge (#20)

--- scripts/publish_server.sh
+++ scripts/publish_server.sh
@@ -3,13 +3,9 @@ set -e
 
 REPO_BASE=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 GO_ROOT="${REPO_BASE}/go/server/"
-GCLOUD_SERVICE_ACCOUNT_KEY_JSON=${HOME}/gcloud-service-account-key.json
 
-echo $GCLOUD_SERVICE_ACCOUNT_KEY | base64 --decode -i > $GCLOUD_SERVICE_ACCOUNT_KEY_JSON
-gcloud auth activate-service-account --key-file $GCLOUD_SERVICE_ACCOUNT_KEY_JSON
+gcloud --quiet config set compute/zone $GCE_INSTANCE_ZONE
+gcloud --quiet config set project $GCE_PROJECT
 
-gcloud --quiet config set compute/zone $GCLOUD_COMPUTE_ZONE
-gcloud --quiet config set project $PROJECT_NAME
-
-gcloud builds submit --tag gcr.io/trial-194900/go-werewolf:latest $GO_ROOT
-gcloud compute instances reset $GCLOUD_VM_INSTANCE
+gcloud builds submit --tag gcr.io/$GCE_PROJECT/go-werewolf:latest $GO_ROOT
+gcloud compute instances reset $GCE_INSTANCE
