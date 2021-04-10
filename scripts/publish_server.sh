8355b868867c2c3b46ada34fa69bdd498e7de5d7
Lan Xiao <lx223@users.noreply.github.com>
2021-04-10 17:18:57 +0100

    Add log lines in server publish script (#46)

--- scripts/publish_server.sh
+++ scripts/publish_server.sh
@@ -7,5 +7,8 @@ GO_ROOT="${REPO_BASE}/go/server/"
 gcloud --quiet config set compute/zone $GCE_INSTANCE_ZONE
 gcloud --quiet config set project $GCE_PROJECT
 
+echo "Building and uploading new server image"
 gcloud builds submit --tag gcr.io/$GCE_PROJECT/go-werewolf:latest $GO_ROOT
+
+echo "Resetting cloud VM instance"
 gcloud compute instances reset $GCE_INSTANCE
